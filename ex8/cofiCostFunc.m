12b04a6e8e6ac6873e484ede681fe95cb46307d9
Luis Solís <37953927+solisgb@users.noreply.github.com>
2021-03-23 18:59:24 +0100

    Add files via upload

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,20 +40,15 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum((R == 1) .* ((X * Theta' - Y) .^ 2))) / 2;
 
+X_grad = (R == 1) .* (X * Theta' - Y) * Theta + lambda * X;
 
+Theta_grad = (R==1)' .* (X * Theta' - Y)' * X + lambda * Theta;
 
+j_reg_term = lambda * (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2))) / 2;
 
-
-
-
-
-
-
-
-
-
-
+J = J + j_reg_term;
 
 % =============================================================
 
